1cb9df92097e63867ca771b69c162686a7b595b4
Oussama Zaki <oussama.zaki@klarna.com>
2019-02-03 23:50:12 +0100

    🎄 complete day4 solution

--- adventOfCode2018/day4/day4.cpp
+++ adventOfCode2018/day4/day4.cpp
@@ -13,6 +13,7 @@
 #include <algorithm>
 #include <unordered_map>
 #include <array>
+#include <numeric>
 #include <regex>
 
 using namespace std;
@@ -22,7 +23,41 @@ using namespace std;
   chrono::duration<double> elapsed = std::chrono::high_resolution_clock::now() - start; \
   cout << "Execution time: " << elapsed.count() << " s\n";
 
-vector<string> get_input()
+unordered_map<int, std::array<int, 60>> guards;
+
+void construct_timetable(vector<string> inputs)
+{
+  smatch m;
+  int guard_id, start = 0, end = 0;
+  for (const auto &l : inputs)
+  {
+    switch (l[19])
+    {
+      case 'G':
+      {
+        regex_match(l, m, regex(R"(.*Guard #(\d+) begins shift)"));
+        guard_id = stoi(m[1]);
+        break;
+      }
+      case 'w':
+      {
+        regex_match(l, m, regex(R"(.*:(\d+)\] wakes up)"));
+        end = stoi(m[1]);
+        for (int t = start; t < end; t++)
+          guards[guard_id].at(t)++;
+        break;
+      }
+      case 'f':
+      {
+        regex_match(l, m, regex(R"(.*:(\d+)\] falls asleep)"));
+        start = stoi(m[1]);
+        break;
+      }
+    }
+  }
+}
+
+void get_input() 
 {
   vector<string> inputs;
   string line;
@@ -31,54 +66,45 @@ vector<string> get_input()
     inputs.push_back(line);
   sort(inputs.begin(), inputs.end());
 
-  return inputs;
+  construct_timetable(inputs);
 }
 
-int day4_part1(vector<string> inputs)
+int day4_part1()
 {
-  unordered_map<int, std::array<int, 60>> guards;
+  // Find laziest guard, i.e. the one that slept most minutes
+  int lazy_guard = 0, most_minutes = 0;
+  for (auto &it : guards)
+  {
+    int total_minutes = accumulate(it.second.begin(), it.second.end(), 0);
+    if (total_minutes > most_minutes)
+      most_minutes = total_minutes;
+      lazy_guard = it.first;
+  }
+  // Get the most slept-during minute
+  int max_minute = distance(guards[lazy_guard].begin(), max_element(guards[lazy_guard].begin(), guards[lazy_guard].end()));
 
-  smatch m;
-  int guard_id, start = 0, end = 0;
-  for (const auto &l : inputs) {   
-    switch (l[19]) {
-      case 'G': {
-        regex_match(l, m, regex(R"(.*Guard #(\d+) begins shift)"));
-        guard_id = stoi(m[1]);
-        break;
-      }
-      case 'w': {
-        regex_match(l, m, regex(R"(.*:(\d+)\] wakes up)"));
-        end = stoi(m[1]);
-        for (int t = start; t < end; t++) {
-          guards[guard_id].at(t)++;
-        }
+  return lazy_guard * max_minute;
+}
 
-        break;
-      }
-      case 'f': {
-        regex_match(l, m, regex(R"(.*:(\d+)\] falls asleep)"));
-        start = stoi(m[1]);
-        break;
-      }
+int day4_part2()
+{
+  // 2 dimential max, first by guard, then max from all guards.
+  int guard = 0, minute = 0, absolute_max_minute = 0;
+  for (auto &it : guards)
+  {
+    int* max_minute_by_guard = max_element(it.second.begin(), it.second.end());
+    if (*max_minute_by_guard > absolute_max_minute) {
+      guard = it.first;
+      absolute_max_minute = *max_minute_by_guard;
+      minute = distance(guards[guard].begin(), max_minute_by_guard);
     }
   }
-
-  // TODO: Find guard with max sum(minutes)
-  // TODO: Find max minute within that guard shift table
-
-  return 0;
-}
-
-int day4_part2(vector<string> input)
-{
-
-  return 0;
+  return guard * minute;
 }
 
 int main()
 {
-  vector<string> input = get_input();
-  cout << day4_part1(input) << endl;
-  cout << day4_part2(input) << endl;
+  get_input();
+  cout << day4_part1() << endl;
+  cout << day4_part2() << endl;
 }
