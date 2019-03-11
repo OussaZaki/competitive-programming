aabd994a3f0990777e9894234ce803569b240c3d
Oussama Zaki <oussama.zaki@klarna.com>
2019-03-11 23:46:58 +0100

    🎄 y.18 day15 solution [wip]

---
+++ adventOfCode2018/day15/day15.cpp
@@ -0,0 +1,53 @@
+/**
+ * C++ solution for Day15 - Advent of code 2018
+ *
+ * link - https://adventofcode.com/2018/day/15
+ * author - Oussa Zaki <zaki.oussama@gmail.com>
+ */
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <chrono>
+#include <algorithm>
+#include <regex>
+
+using namespace std;
+
+#define START_TIMING auto start = std::chrono::high_resolution_clock::now();
+#define END_TIMING \
+  chrono::duration<double> elapsed = std::chrono::high_resolution_clock::now() - start; \
+  cout << "Execution time: " << elapsed.count() << " s\n";
+
+vector<int> get_input()
+{
+  vector<int> inputs;
+  string line;
+
+  while (getline(cin, line))
+  {
+    // TODO parse input
+    inputs.push_back(0);
+  }
+  return inputs;
+}
+
+int day15_part1(vector<int> input)
+{
+  
+  return 0;
+}
+
+int day15_part2(vector<int> input)
+{
+  
+  return 0;
+}
+
+int main()
+{
+  vector<int> input = get_input();
+  cout << day15_part1(input) << endl;
+  cout << day15_part2(input) << endl;
+}
