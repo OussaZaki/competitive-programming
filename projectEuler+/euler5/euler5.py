6ce472bb0168b48a29b48f87a72bc84a17a10819
Anass Zaki <zaki.oussama@gmail.com>
2019-05-09 23:44:46 +0200

    ➗ projectEuler+ 5 lcm brute force

--- projectEuler+/euler5/euler5.py
+++ projectEuler+/euler5/euler5.py
@@ -1,7 +1,16 @@
-def smallest_multiple(): 
-    return 0
+#!/bin/python3
+
+import functools
+
+def gcd(x, y):
+    while y != 0:
+        x, y = y, x % y
+    return x
+
+def smallest_multiple(n): 
+    return int(functools.reduce(lambda x,y: x*y/gcd(x, y), range(1, n + 1)))
 
 T = int(input().strip())
 for i in range(T):
     N = int(input().strip())
-    print(smallest_multiple())
+    print(smallest_multiple(N))
