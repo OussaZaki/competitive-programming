5020b366b09a8e0d925296b6382548ff81775b66
Oussama Zaki <oussama.zaki@klarna.com>
2019-05-11 23:52:29 +0200

    ➗ projectEuler+ 8

---
+++ projectEuler+/euler8/euler8.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+from operator import mul
+from functools import reduce
+
+def max_product(n):
+    digits = [int(x) for x in str(n)]
+    products = []
+    for i in range(0, len(digits) - k):
+        products.append(reduce(mul, digits[i:i+k]))
+    return max(products)
+
+t = int(input().strip())
+for _ in range(t):
+    n, k = input().strip().split(' ')
+    n, k = [int(n), int(k)]
+    num = input().strip()
+    
+    print(max_product(num))
