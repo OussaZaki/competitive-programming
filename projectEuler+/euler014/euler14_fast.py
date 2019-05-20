0d8fec922ad85de6cf325d4c1f18bd32c6986a17
Oussama Zaki <oussama.zaki@klarna.com>
2019-05-21 00:56:31 +0200

    ➗ projectEuler+ 14 recursion is somehow faster

--- projectEuler+/euler014/euler14_fast.py
+++ projectEuler+/euler014/euler14_fast.py
@@ -1,29 +1,25 @@
+#!/bin/python3
+
 limit = 5000001
-collatz = [0] * limit
-max_length = [0] * limit
 
-def precalculate_collatz (n):
-    for i in range(1, limit):
-        num = i
-		counter = 1
-		while num != 1:
-			if num < MAXN and collatz[num] != 0:
-				counter += collatz[num] - 1
-				break
-			if num & 1:
-                num = 3 * num + 1
-			else:
-                num >>= 1
-			counter += 1
+collatz_len = [0, 1] + [0] * limit
+max_len = [0, 1] + [1] * limit
 
-		collatz[i] = counter
-        if (collatz[max_length[i - 1]] > counter) :
-            max_length[i] = max_length[i-1]
-        else:
-            max_length[i] = i
+def collatz(n):
+    if n < limit and collatz_len[n] != 0:
+        return collatz_len[n]
+    else:
+        length = 1 + collatz(3 * n + 1 if n % 2 else n >> 1)
+        if n < limit: collatz_len[n] = length
+        return length
 
+def precompute_max_length():
+    for i in range(2, limit):
+        len_i = collatz(i)
+        max_len[i] = i if len_i >= collatz_len[max_len[i - 1]] else max_len[i - 1]
 
 T = int(input().strip())
+precompute_max_length()
 for _ in range(T):
     N = int(input().strip())
-    print(max_length[N])
+    print(max_len[N])
