2585a656d1d559523134c6371dbe6dea1d7b2521
Oussama Zaki <oussama.zaki@klarna.com>
2019-05-30 23:39:52 +0200

    ➗ projectEuler+ 25 constant time solution

---
+++ projectEuler+/euler025/euler25_constant_time.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+from math import ceil, log,
+
+phi = (1 + 5 ** 0.5) / 2
+
+
+def first_fib_with_N_digits(n):
+    return int(ceil((log(10) * (n - 1) + log(5) / 2) / Math.log(phi)))
+
+
+T = int(input().strip())
+for _ in range(T):
+    N = int(input())
+    print(first_fib_with_N_digits(N))
